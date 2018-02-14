baa69612767f608f6018ee1cd48f0a3d46bba80a
Ali Zain Prasla <alizainprasla@axact.com>
2018-02-14 19:35:53 +0500

    Initial Commit

---
+++ ReachabilityTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReachabilityTest
+//
+//  Created by Ali Zain Prasla on 2/14/18.
+//  Copyright © 2018 Ali Zain Prasla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
